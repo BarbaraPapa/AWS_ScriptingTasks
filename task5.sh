ee87400d52643a6482451af0e5b11c2e21a6c5a0
Barbara Papa <103266205+BarbaraPapa@users.noreply.github.com>
2024-12-08 21:06:08 +0100

    fixed issue task5.sh script

--- task5.sh
+++ task5.sh
@@ -5,6 +5,13 @@ read dir
 echo "Enter the number of files to create:"
 read num_files
 
+#create directory if it doesn't exist
+if [ ! -d "$dir" ]; then
+    mkdir -p "$dir"
+    echo " $dir directory has been created "
+fi
+
+#create files
 for ((i=1; i<=num_files; i++))
 do
     touch "$dir/file$i.txt"
